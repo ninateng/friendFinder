21a9fa8cd585861e773def8fb7e7632ba085d958
ninateng <niinateng@gmail.com>
2019-07-03 11:11:52 -0700

    running local server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+var app = express();
+
+var PORT = 3001;
+
+app.get('/', function (req,res) {
+    res.send('hello joe')
+})
+
+app.listen( PORT, function () {
+    console.log('listening on ' + PORT)
+})
